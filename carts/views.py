b62df2cb979e4e94edabd4c2887f3a315a7d92b2
haskell <hask777>
2018-12-07 21:32:30 +0300

    run

--- carts/views.py
+++ carts/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None: #and isinctance(cart_id, int):
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print('Cart exists')
 
-    # key = request.session.session_key
-    # print(key)
-    request.session['first_name'] = "hask777"
+    request.session['user'] = request.user.username
     return render(request, "carts/home.html", {})
